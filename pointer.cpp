96479d9c0d2573de03aa871dfde3a17d9e70ccca
abhinav3593 <163002463+abhinav3593@users.noreply.github.com>
2024-08-13 23:27:00 +0530

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+void solve(vector<int> v){
+    cout<<sizeof(v)<<endl;
+}
+
+int main(){
+    vector<int> v;
+    v.push_back(10);f
+    v.push_back(20);
+    v.push_back(30);
+    solve(v);
+
+    
+    return 0;
+}
